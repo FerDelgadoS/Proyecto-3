085739cee66a52ede0158bcfb0e319fce91053c1
FerDelgadoS <89162996+FerDelgadoS@users.noreply.github.com>
2021-11-22 16:34:55 -0600

    modificacion comentarios

--- proyecto3Sp32/proyecto3Sp32.ino
+++ proyecto3Sp32/proyecto3Sp32.ino
@@ -8,7 +8,7 @@
 //*****************************************************************************
 // Librerías
 //*****************************************************************************
-#include <Arduino.h>       // se incluye la librereria de arduino
+#include <Arduino.h>      
 #include <Separador.h>
 //librerías de NEOPIXEL
 #include <Adafruit_NeoPixel.h>
@@ -32,17 +32,17 @@ MAX30105 particleSensor;
 //*****************************************************************************
 // Prototipos de función
 //*****************************************************************************
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);//esto indica cuantos neopixel son se puede cambiar dependiendo la cantidad 
 void sensorMAX30105(void);
 //*****************************************************************************
 // Variables Globales
 //*****************************************************************************
 // Create An LCD Object. Signals: [ RS, EN, D4, D5, D6, D7 ]
-//se crea la libreria para inicializar el LCD
-uint8_t voltaje3; //se nombra a una variable la cual recibira el texto de la comunicacion USART
 
-int adcRaw;    //la netrada del sensor analogico
-float voltaje; //la medida que se tendra con decimales del sensor
+uint8_t voltaje3; //comunicacion USART
+
+int adcRaw;    
+float voltaje; 
 
 //filtro para el sensor
 float adcFiltradoEMA = 0; // S(0) = Y(0)
@@ -66,14 +66,14 @@ uint32_t irBuffer[100]; //infrared LED sensor data
 uint32_t redBuffer[100];  //red LED sensor data
 #endif
 
-int32_t bufferLength; //Tamaño del dato
-int32_t spo2; //Valor SPO2
-int8_t validSPO2; //Indicador para ver si el valor del SPO2 es valido
-int32_t heartRate; //valor del Ritmo Cardíaco 
-int8_t validHeartRate; //Indicador para ver si el valor del Ritmo cardíaco es valido
+int32_t bufferLength; // dato
+int32_t spo2; //SPO2
+int8_t validSPO2; //valor del SPO2 es valido
+int32_t heartRate; //Ritmo Cardíaco 
+int8_t validHeartRate; /Ritmo cardíaco es valido
 
-byte pulseLED = 11; //debe estar en un pin PWM
-byte readLED = 13; //Parpadea con cada medición de dato
+byte pulseLED = 11; 
+byte readLED = 13; 
 
 String dato ="";
 String  HR = ""; 
@@ -89,21 +89,22 @@ Separador s;
 //*****************************************************************************
 void setup()
 {
-  //se iniciara las dos comunicasiones sriales donde unsa sera con el monitor del programa y la otra con la Tiva C
-  Serial.begin(115200);
+
+  Serial.begin(115200);//se inciia los dos monitores seriales para envio y docuemntacion de datos 
   Serial2.begin(115200);
   //Configuración MAX30105
   pinMode(pulseLED, OUTPUT);
   pinMode(readLED, OUTPUT);
+  
   // Initialización para MAX30105
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Usa el I2C default, velocidad de 400kHz 
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //se inicializa la velocidad del sensor 
   {
-    Serial.println(F("No se encontró sensor MAX30105, Chekear conexión"));
+    Serial.println(F("No se encontró sensor MAX30105, mala conexión"));
     while (1);
   }
 
-  Serial.println(F("coloque el dedo en sensor e ingrese cualquier dato al monitor"));
-  while (Serial.available() == 0) ; //espera a que el usuario ingrese cualquier letra al monitor
+  Serial.println(F("coloque el dedo en sensor e ingrese dato al monitor"));
+  while (Serial.available() == 0) ; //se ha iniciado el monitor, si o no?
   Serial.read();
 
   byte ledBrightness = 60; //Configuración: 0=Off a 255=50mA
@@ -112,6 +113,7 @@ void setup()
   byte sampleRate = 100; //Configuración: 50, 100, 200, 400, 800, 1000, 1600, 3200
   int pulseWidth = 411; //Configuración: 69, 118, 215, 411
   int adcRange = 4096; //Configuración: 2048, 4096, 8192, 16384
+  
   //Configuración del sensor MAX30105
    particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
 
@@ -135,71 +137,71 @@ void loop()
 }
 
 //---------------------------------------------------------------------------------------------------------------------
-//Lectura de datos Sensor MAX30105
+//Sensor MAX30105
 //---------------------------------------------------------------------------------------------------------------------
 void sensorMAX30105(void){
-  //Tamaño del buffer de 100 para que durante 4 segundos almacene las muestres a 25sps
+  //Tamaño del buffer de 100 para alacenamiento de datos 
    bufferLength = 100; 
 
-   //lee las primeras 100 muestras y determina el rango de la señal
+   //lee 100 muestras y determina el rango de la señal
   for (byte i = 0 ; i < bufferLength ; i++){
 
-  //Se tienen nuevos datos?
+  
   while (particleSensor.available() == false)
-  //Mira si el sensor tiene nuevos datos  
-      particleSensor.check(); 
+  
+      particleSensor.check(); //variable para inicalizar datos 
 
   redBuffer[i] = particleSensor.getRed();
   irBuffer[i] = particleSensor.getIR();
-  particleSensor.nextSample(); //Se acabo con estra muestra y se pasa a la siguiente
+  particleSensor.nextSample(); //siguiente muestra 
 
-  Serial.print(F("red="));
+  Serial.print(F("red="));//despliegue de valores del buffer
   Serial.print(redBuffer[i], DEC);
   Serial.print(F(", ir="));
   Serial.println(irBuffer[i], DEC);
 
   }
-  //Calcula el nivel SPO2 y el Ritmo cardíaco después de 100 muestras (los primeros 4 segundos de muestras)
+  //Calcula despues de 100 muestras los valores 
   maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
-   //se estan tomando las medidas constantemente y se calculan cada segundo
+
   while (1){
-    //Se colocan las primeras 25 muestras en la memoria y las 75 siguientes en la cima. 
+    //se colocan 25 muestras y otras 75 
     for (byte i = 25; i < 100; i++)
     {
       redBuffer[i - 25] = redBuffer[i];
       irBuffer[i - 25] = irBuffer[i];
     }
 
-    //toma 25 muestras antes de calcular el pulso cardíaco. 
+    //restar 25 muestras para calculos 
     for (byte i = 75; i < 100; i++)
     {
-      //Se tienen nuevos datos? 
+      //ver nuevos datos 
       while (particleSensor.available() == false) 
-      //Mira si el sensor tiene nuevos datos
+      //repetir los datos y verificar que no haya mas datos 
         particleSensor.check(); 
 
       digitalWrite(readLED, !digitalRead(readLED)); //Parpadea con la led cada que ingresa nuevos datos
 
       redBuffer[i] = particleSensor.getRed();
       irBuffer[i] = particleSensor.getIR();
-      particleSensor.nextSample(); //Se termina con esta muestra y se mueve a la siguiente
+      particleSensor.nextSample(); //se mueve a la siguiente
 
       Serial2.print(heartRate, DEC);
       Serial2.print(",");
       Serial2.println(spo2,DEC);
 
      if (Serial2.available()>0){
-      rainbowCycle(2);
-      //Se envian las muestras y los calculos al monitor
-      //Lee el dato hasta que hay un enter
-      dato = Serial2.readStringUntil('\n');
+      rainbowCycle(2);//ciclo para neopixel 
+      
+     //se incializa la lectura de datos desde la tiva 
+      dato = Serial2.readStringUntil('\n');//leer el dato hasta que haya un enter 
       //Separa el primer dato del primer indicador, en este caso la ',' y el primer dato se indica con el 0
       HR = s.separa(dato, ',',0);
       //Separa el segundo dato con el indicador ',' en este caso se indica con el 1
       SPO2 = s.separa(dato,',',1);
-      //Se imprimern los datos obtenidos de la lectura.
-      Serial.println("Heart rate= "+HR);
+     
+      Serial.println("Heart rate= "+HR);//mostrar los datos obtenidos 
       Serial.println("Oximetría= "+SPO2);
       }
      }
@@ -211,7 +213,7 @@ void sensorMAX30105(void){
 //---------------------------------------------------------------------------------------------------------------------
 //Funciones para Neopixel
 //---------------------------------------------------------------------------------------------------------------------
-// Secuencia de leds 1 encedido otro apagado de 1 en 1 de un solo color
+// Secuencia de leds para orden 
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
@@ -219,7 +221,7 @@ void colorWipe(uint32_t c, uint8_t wait) {
     delay(wait);
   }
 }
-//Arcoiris
+//funcion para el movimiento 
 void rainbow(uint8_t wait) {
   uint16_t i, j;
 
@@ -232,11 +234,11 @@ void rainbow(uint8_t wait) {
   }
 }
 
-// Arcoiris girando en el neopixel
+// Arcoiris neopixel
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
 
-  for(j=0; j<256*2; j++) { // 5 ciclos de todos los colores en la rueda
+  for(j=0; j<256*2; j++) { 
     for(i=0; i< strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
@@ -245,44 +247,43 @@ void rainbowCycle(uint8_t wait) {
   }
 }
 
-//recorrido de colores a traves de leds
+//recorrido colores leds
 void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //10 ciclos de seguimiento
+  for (int j=0; j<10; j++) { 
     for (int q=0; q < 3; q++) {
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //enciende cada 3 leds
+        strip.setPixelColor(i+q, c);   
       }
       strip.show();
 
       delay(wait);
 
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //apaga cada 3 leds
+        strip.setPixelColor(i+q, 0);      
       }
     }
   }
 }
 
-//recorrido de arcoiris a traves de los leds
+
 void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // ciclo de los 256 colores en toda la rueda
+  for (int j=0; j < 256; j++) {    
     for (int q=0; q < 3; q++) {
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //enciende cada 3 leds
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    
       }
       strip.show();
 
       delay(wait);
 
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        strip.setPixelColor(i+q, 0);       
       }
     }
   }
 }
 
-// entrada de 0 a 255 para tener un color específico 
-//los colores van de orden R G B empezando desde R
+
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
